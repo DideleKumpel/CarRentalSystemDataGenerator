9123acd1381259eb60aa698c1fc3e66a5872725c
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-03-24 11:04:42 +0100

    Add interface to service for DB operations

---
+++ CarRentalSystemDataGenerator/Services/DbServiceInterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRentalSystemDataGenerator.Services
+{
+    internal interface DbServiceInterface<T>
+    {
+        //Get
+        T Get(int id);
+        List<T> GetAll();
+        List<T> GetByFilter(Func<T, bool> filter);
+        T Get(T item);
+        bool Exists(int id);
+        bool Exclude(T item);
+
+        //Create
+        T Add(T item);
+        int AddMany(List<T> items);
+
+        //Update
+        T Update(T item);
+        T Update(int id, T item);
+        T UpdateMany(List<T> items);
+
+        //Delete
+        bool Delete(int id);
+        bool Delete(T item);
+
+        //Transations
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+    }
+}
--- CarRentalSystemDataGenerator/ViewModel/GeneratorViewModel.cs
+++ CarRentalSystemDataGenerator/ViewModel/GeneratorViewModel.cs
@@ -8,8 +8,32 @@ namespace CarRentalSystemDataGenerator.ViewModel
     internal partial class GeneratorViewModel: ObservableObject
     {
         public GeneratorViewModel() { }
+
+        //Generator flags
         [ObservableProperty]
-        private int chujWdupie;
+        private bool _adressesIsChecked;
+        [ObservableProperty]
+        private bool _brandsIsChecked;
+        [ObservableProperty]
+        private bool _carsIsChecked;
+        [ObservableProperty]
+        private bool _customersIsChecked;
+        [ObservableProperty]
+        private bool _employeesIsChecked;
+        [ObservableProperty]
+        private bool _maintenancesIsChecked;
+        [ObservableProperty]
+        private bool _modelsIsChecked;
+        [ObservableProperty]
+        private bool _officesIsChecked;
+        [ObservableProperty]
+        private bool _paymentsIsChecked;
+        [ObservableProperty]
+        private bool _rentalsIsChecked;
+
+        [ObservableProperty]
+        private int _numOfRecordToGenreate;
+
 
     }
 }
