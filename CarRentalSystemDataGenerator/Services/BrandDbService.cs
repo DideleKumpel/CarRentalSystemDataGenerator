8445b30d54728cd4c7f2fd38d82718bcb083425b
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-04-02 23:44:50 +0200

    BrandDbService impelmented

--- CarRentalSystemDataGenerator/Services/BrandDbService.cs
+++ CarRentalSystemDataGenerator/Services/BrandDbService.cs
@@ -38,24 +38,174 @@ namespace CarRentalSystemDataGenerator.Services
             }
             return found;
         }
-        bool Exists(int id);
+        bool Exists(int id)
+        {
+            Brand found = base._db.Brands.FirstOrDefault(b => b.BrandID == id);
+            if (found == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
         //Create
-        T Add(T item);
-        int AddMany(List<T> items, bool CancelOnError);
+        Brand Add(Brand item)
+        {
+            
+            if (item == null)
+            {
+                throw new Exception("Brand cannot be null.");
+            }
+            base.BeginTransaction();
+            try
+            {
+                base._db.Brands.Add(item);
+                base._db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                base.RollbackTransaction();
+                throw new Exception($"Error adding brand: {ex.Message}");
+            }
+            return item;
+        }
+        int AddMany(List<Brand> items, bool CancelOnError) {
+            base.BeginTransaction();
+            int successCount = 0;
+            foreach (Brand item in items)
+            {
+                try
+                {
+                    if (item == null)
+                    {
+                        throw new Exception("Brand cannot be null.");
+                    }
+                    Add(item);
+                    successCount++;
+                }
+                catch (Exception ex)
+                {
+                    if (CancelOnError)
+                    {
+                        base.RollbackTransaction();
+                        throw new Exception($"Error adding brand: {ex.Message}");
+                    }
+                }
+            }
+            base._db.SaveChanges();
+            return successCount;
+        }
 
         //Update
-        T Update(T item);
-        T Update(int id, T item);
-        int UpdateMany(List<T> items, bool CancelOnError);
+        Brand Update(Brand item)
+        {
+            if (item == null)
+            {
+                throw new Exception("Brand cannot be null.");
+            }
+            Brand found = base._db.Brands.FirstOrDefault(b => b.BrandID == item.BrandID);
+            if (found == null)
+            {
+                throw new Exception($"Brand with id {item.BrandID} not found.");
+            }
+            base.BeginTransaction();
+            try
+            {
+                found.Name = item.Name;
+                base._db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                base.RollbackTransaction();
+                throw new Exception($"Error updating brand: {ex.Message}");
+            }
+            return found;
+        }
+        Brand Update(int id, Brand item)
+        {
+            if (item == null)
+            {
+                throw new Exception("Brand cannot be null.");
+            }
+            Brand found = base._db.Brands.FirstOrDefault(b => b.BrandID == id);
+            if (found == null)
+            {
+                throw new Exception($"Brand with id {id} not found.");
+            }
+            base.BeginTransaction();
+            try
+            {
+                found.Name = item.Name;
+                base._db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                base.RollbackTransaction();
+                throw new Exception($"Error updating brand: {ex.Message}");
+            }
+            return found;
+        }
+        int UpdateMany(List<Brand> items, bool CancelOnError)
+        {
+            base.BeginTransaction();
+            int successCount = 0;
+            foreach (Brand item in items)
+            {
+                try
+                {
+                    if (item == null)
+                    {
+                        throw new Exception("Brand cannot be null.");
+                    }
+                    Update(item);
+                    successCount++;
+                }
+                catch (Exception ex)
+                {
+                    if (CancelOnError)
+                    {
+                        base.RollbackTransaction();
+                        throw new Exception($"Error updating brand: {ex.Message}");
+                    }
+                }
+            }
+            base._db.SaveChanges();
+            return successCount;
+        }
 
         //Delete
-        bool Delete(int id);
-        bool Delete(T item);
-
-        //Transations
-        void BeginTransaction();
-        void CommitTransaction();
-        void RollbackTransaction();
+        bool Delete(int id)
+        {
+            base.BeginTransaction();
+            try
+            {
+                base._db.Brands.Remove(base._db.Brands.FirstOrDefault(b => b.BrandID == id));
+                base._db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                base.RollbackTransaction();
+                throw new Exception($"Error deleting brand: {ex.Message}");
+            }
+             return true;
+        }
+        bool Delete(Brand item)
+        {
+            base.BeginTransaction();
+            try
+            {
+                base._db.Brands.Remove(base._db.Brands.FirstOrDefault(b => b.BrandID == item.BrandID));
+                base._db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                base.RollbackTransaction();
+                throw new Exception($"Error deleting brand: {ex.Message}");
+            }
+            return true;
+        }
     }
 }
