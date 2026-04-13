07617f78b41fa72e8d57f55156970ca5cf46f8ce
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-04-13 15:14:28 +0200

    ExportView frontend

---
+++ CarRentalSystemDataGenerator/ViewModel/ExportViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CarRentalSystemDataGenerator.ViewModel
+{
+    internal class ExportViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private string _directory;
+        [ObservableProperty]
+        private string _fileName;
+        [ObservableProperty]
+        private bool _xmlSelected;
+
+
+    }
+}
